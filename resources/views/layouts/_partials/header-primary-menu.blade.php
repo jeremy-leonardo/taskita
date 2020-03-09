81aa89e20db5f88bb4d4c00ae73e9fa0ee37d61f
Jeremy Leonardo <jeremy.leonardo@atomic.id>
2020-03-09 08:30:56 +0700

    change header nav to bootstrap responsive nav

--- resources/views/layouts/_partials/header-primary-menu.blade.php
+++ resources/views/layouts/_partials/header-primary-menu.blade.php
@@ -1,9 +1,9 @@
-<div class="row">
+{{-- <div class="row">
     <div class="col-md-12 primary-navigation">
         <div class="row">
             <div class="col-md-4">
                 <a href="/">
-                    <img src="image/logo-primary.svg" alt="" class="logo-header">
+                    <img src="{{asset('image/logo-primary.svg')}}" alt="" class="logo-header">
                 </a>
             </div>
             <div class="col-md-8">
@@ -20,4 +20,25 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+<nav class="navbar navbar-expand-lg navbar-light primary-navigation">
+    <a class="navbar-brand mr-5" href="#"><img src="{{asset('image/logo-primary.svg')}}" alt="" class="logo-header"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse mt-2" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link deco-none mr-3" href="/">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link deco-none mr-3" href="/catalog">CATALOG</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link deco-none mr-3" href="/login">LOGIN</a>
+        </li>
+      </ul>
+    </div>
+</nav>
